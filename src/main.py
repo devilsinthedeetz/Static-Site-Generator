f0a74c818d71459568ec428bfa4b447d7a6b3052
devilsinthedeetz <andrew@devilsinthedeetz.us>
2026-07-10 19:47:42 -0600

    recursive function to copy files from static dir

--- src/main.py
+++ src/main.py
@@ -1,11 +1,47 @@
-from textnode import TextNode, TextType
+import os
+from os.path import isfile
+import shutil
+
+
+def copy_static_to_public(source_path: str, dest_path: str, deleted: bool):
+    if not deleted:
+        if os.path.exists("public"):
+            shutil.rmtree("public")
+            deleted = True
+            os.mkdir("public")
+        else:
+            deleted = True
+            os.mkdir("public")
+    source_dir: list[str] = os.listdir(source_path)
+    if not source_dir:
+        return
+    for path in source_dir:
+        if os.path.exists(os.path.join(dest_path, path)):
+            continue
+        else:
+            if os.path.isfile(os.path.join(source_path, path)):
+                if not os.path.isdir(dest_path):
+                    os.mkdir(dest_path)
+                print(
+                    shutil.copy(
+                        os.path.join(source_path, path), os.path.join(dest_path, path)
+                    )
+                )
+            else:
+                if os.path.isdir(os.path.join(source_path, path)):
+                    if not os.path.exists(dest_path):
+                        os.mkdir(dest_path)
+                    if not os.path.exists(os.path.join(dest_path, path)):
+                        os.mkdir(os.path.join(dest_path, path))
+                    copy_static_to_public(
+                        os.path.join(source_path, path),
+                        os.path.join(dest_path, path),
+                        deleted,
+                    )
 
 
 def main():
-    my_node = TextNode(
-        "This is some anchor text", TextType.LINK, "https://www.boot.dev"
-    )
-    print(my_node)
+    copy_static_to_public("static", "public", False)
 
 
 main()
